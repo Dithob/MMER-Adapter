623442c57eb1daf87f87e4f50e437ddf5356df55
Dithob <2508807574@qq.com>
2026-04-01 11:12:44 +0800

    update: 更新了几条训练日志

--- config/config_classification.py
+++ config/config_classification.py
@@ -120,8 +120,8 @@ class ConfigClassification():
                     'pseudo_tokens': 2,
                     'label_index_mapping': {'neutral': 0, 'surprise': 1, 'fear': 2, 'sadness': 3, 'joy': 4, 'disgust': 5,
                                            'anger': 6},
-                    # 'batch_size': 12,
-                    'batch_size': 20,
+                    'batch_size': 12,
+                    # 'batch_size': 20,
                     'learning_rate': 5e-4,
                     # feature subNets
                     'a_lstm_hidden_size': 32,
